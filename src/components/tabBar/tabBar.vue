ed17312ca4555945b5a4725a08c17b196b033da8
UNKLAT <1728548869@qq.com>
2022-09-29 18:22:37 +0800

    tab导航

---
+++ src/components/tabBar/tabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tab-bar">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'tabBar',
+  props: {
+
+  },
+
+  
+}
+
+</script>
+
+<style scoped>
+
+.tab-bar {
+  display: flex;
+  height: 49px;
+  background-color: gray;
+}
+
+</style>
---
+++ src/components/tabBar/tabBarItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tab-bar__item">
+
+    <div class="tab-bar__item-icon" :style="activeStyle">
+      <slot name="icon"> </slot>
+    </div>
+
+    <div class="tab-bar__item-title" :style="activeStyle">
+      <slot name="title"> </slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'tabBarItem',
+  props: {
+    activeColor: {
+      type: String,
+      default: 'red',
+    }
+  },
+
+  computed: {
+    activeStyle() {
+      return 'color:' + this.activeColor
+    },
+  },
+}
+
+</script>
+
+<style scoped>
+  .tab-bar__item {
+    flex: 1;
+    text-align: center;
+  }
+
+</style>
